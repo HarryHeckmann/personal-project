39cdc4a9aec2d1a8d14e2846a8c26c0222557af1
Harry Heckmann <harry@MacBook-Pro.local>
2019-03-04 15:13:07 -0600

    updates index.html

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const path = require('path'); // Usually moved to the start of fil
 const cors = require('cors')
 const express = require("express");
 const { json } = require("body-parser");
